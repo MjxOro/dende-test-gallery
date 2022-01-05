c60fedb4cfccc98b25df0385060b81b784969295
Matthew Oro <MatthewEOro@gmail.com>
2022-01-05 17:18:32 -0500

    update package.json

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
+require("dotenv").config;
 const path = require("path");
 const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname, "..", "client", "public");
 const port = process.env.PORT || 8080;
-app.use(express.static(publicPath));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
-});
+
+if (process.env.NODE_ENV === "production") {
+  // Handle React routing, return all requests to React app
+  app.use(express.static(path.join(__dirname, "..", "client", "build")));
+  app.get("*", (_, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "..", "client", "build", "index.html")
+    );
+  });
+}
 app.listen(port, () => {
   console.log("Server is up!");
 });
